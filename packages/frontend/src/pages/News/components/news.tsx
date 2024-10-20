e8fca3a04a1ef30842892da651655abe51725e41
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2024-10-21 00:34:37 +0530

    [Added] lerna Monorepo

--- packages/frontend/src/pages/News/components/news.tsx
+++ packages/frontend/src/pages/News/components/news.tsx
@@ -3,17 +3,6 @@ import { getNewsList } from "../../../services/news";
 import NewsItem from "./newsItem";
 import { Box, Grid } from "@chakra-ui/react";
 
-const news = {
-  title: "Asian IPOs set for blockbuster week as ‘Animal Spirits’ return",
-  description:
-    "Asia's stock markets are about to witness the busiest week of public offerings in over two years. Around 20 companies will list shares, aiming to raise $8.3 billion. Companies from China, India, and Japan are among the major participants.",
-  image:
-    "https://img.etimg.com/thumb/msid-114388871,width-1200,height-630,imgsize-43128,overlay-etmarkets/articleshow.jpg",
-  publishedAt: "2024-10-20 03:34:09",
-  source: "The Economic Times",
-  url: "https://economictimes.indiatimes.com/markets/ipos/fpos/asian-ipos-set-for-blockbuster-week-as-animal-spirits-return/articleshow/114388435.cms",
-};
-
 function NewsList() {
   const [news, setNews] = useState([]);
 
